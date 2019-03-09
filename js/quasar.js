e30c306b0091e6651b3f48cbb1cd114d2a35665f
davramov <davs.abramov@gmail.com>
2019-03-09 12:53:02 -0800

    small dataset for performance testing

--- js/quasar.js
+++ js/quasar.js
@@ -132,15 +132,15 @@ function updateDepthDomain(min, max){
 
 function loadGalaxyData(callback) {
 	//loadProjections()
-
-	d3.json('data/galaxies_bigger.json').then(function(d){
+	/*
+	d3.json('data/galaxies.json').then(function(d){
 		galaxies = d
 		processGalaxyData(galaxies);
 		//galaxies = data; // TODO: as new Map()
 		callback();
 	//}).then((data) => {
 	});
-	/*
+	*/
 	d3.dsv(" ", galaxyFile, (d) => {
 		return {
 			'NSAID': d.NSAID,
@@ -160,7 +160,7 @@ function loadGalaxyData(callback) {
 		processGalaxyData(data);
 		galaxies = data; // TODO: as new Map()
 		callback();
-	});*/
+	});
 }
 
 
@@ -203,7 +203,7 @@ function lookUp(redshift){
 }
 
 function loadCloseImpactLookUp(){
-	d3.dsv(' ', 'data/galaxyCloseImpactLookup_bigger.dat').then(function(d){
+	d3.dsv(' ', 'data/galaxyCloseImpactLookup.dat').then(function(d){
 		impactParameters = d
 	})
 }
@@ -368,7 +368,7 @@ function loadSkewerData(callback) {
 
 			// individual reads of each element
 			spectra.forEach( (el) => {
-				let path = 'data/spectra_' + el + '_norm_bigger/'
+				let path = 'data/partial/spectra_' + el + '_partial_norm/'
 				d3.dsv(' ', path + file, (d) => {
 					//console.log(d.sig_norm)
 					return {
@@ -1823,8 +1823,8 @@ function plotGalaxyImage(idx){
 	
 		var galaxyImage = svg.append('div')
 		galaxyImage.append('img')
-			//.attr('src', 'data/galaxyImages_partial/' + g.NSAID + '.jpg')
-			.attr('src', 'data/galaxyImages/' + g.NSAID + '.jpg')
+			.attr('src', 'data/partial/galaxyImages_partial/' + g.NSAID + '.jpg')
+			//.attr('src', 'data/galaxyImages/' + g.NSAID + '.jpg')
 			.attr('width', '160px')
 			.on('mouseover', (j) => {			
 				pointOverIdx = idx //;
